75b37f5a497a34f8c9ac1344d26c31c1141cbda1
Pedro X <pxpm88@gmail.com>
2023-06-21 18:53:19 +0100

    remove prism css

--- resources/views/inc/theme_styles.blade.php
+++ resources/views/inc/theme_styles.blade.php
@@ -3,7 +3,6 @@
 @basset('https://unpkg.com/animate.css@4.1.1/animate.compat.css')
 @basset('https://unpkg.com/noty@3.2.0-beta-deprecated/lib/noty.css')
 
-@basset('https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css')
 @basset('https://coreui.io/demos/bootstrap/4.2/free/vendors/simplebar/css/simplebar.css')
 @basset('https://coreui.io/demos/bootstrap/4.2/free/css/vendors/simplebar.css')
 @basset('https://coreui.io/demos/bootstrap/4.2/free/css/style.css')
