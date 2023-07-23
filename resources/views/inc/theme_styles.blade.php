c902299407ee1c9b722fb12d4ad41751b7f5e402
Antonio Almeida <promatik@gmail.com>
2023-07-23 22:06:56 +0100

    Removed unnecessary source-sans-pro font

--- resources/views/inc/theme_styles.blade.php
+++ resources/views/inc/theme_styles.blade.php
@@ -1,9 +1,5 @@
 @basset('https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2/dist/css/coreui.min.css')
 @basset('https://cdn.jsdelivr.net/npm/simplebar@6.2.5/dist/simplebar.min.css')
 
-{{-- Source Sans Font --}}
-@bassetArchive('https://github.com/adobe-fonts/source-sans/releases/download/3.046R/WOFF2-source-sans-3.046R.zip', 'source-sans-pro')
-@basset(base_path('vendor/backpack/theme-coreuiv4/resources/assets/libs/source-sans-pro.css'), 'source-sans-pro/source-sans-pro.css')
-
 {{-- Custom Backpack Rules --}}
 @basset(base_path('vendor/backpack/theme-coreuiv4/resources/assets/css/coreui4.css'))
