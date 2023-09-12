f10b74f951698ecee4b222ecf17e5b4e41ebd3ad
Antonio Almeida <promatik@gmail.com>
2023-09-12 12:57:13 +0100

    Replaced translation keys for error page

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="d-flex align-items-center" style="height: calc(100vh - 7rem)">
   <div class="container">
+    <div class="col-md-6 m-auto">
       <div class="d-flex justify-content-center">
         <h1 class="display-2 mb-0 me-4">{{ $error_number }}</h1>
         <div>
@@ -11,11 +12,12 @@
           <div class="empty-action">
             <a href="./." class="btn btn-primary">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M5 12l14 0"></path><path d="M5 12l6 6"></path><path d="M5 12l6 -6"></path></svg>
-              {{ trans('backpack::base.error.button') }}
+              {{ trans('backpack::base.error_page.button') }}
             </a>
           </div>
         </div>
       </div>
+    </div>
   </div>
 </div>
 @endsection
